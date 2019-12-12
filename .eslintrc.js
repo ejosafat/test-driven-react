299b766e066ec19e098429642f8a5a73e0e1e0da
Eddy Hernandez <eddy.hernandez@immersivelabs.co.uk>
2019-12-12 12:03:58 +0000

    Initial babel setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 6,
+    },
+    env: {
+      node: true,
+    },
+    rules: {
+      quotes: ['error', 'single', {
+        avoidEscape: true
+      }],
+      'comma-dangle': ['error', 'always-multiline'],
+    },
+};
