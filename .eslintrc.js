be18ddaf0905b1a49be2fef4d0069aae6a35b478
Eddy Hernandez <eddy.hernandez@immersivelabs.co.uk>
2019-12-13 11:06:23 +0000

    Initial ESLint and Prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,29 @@
 module.exports = {
-    extends: ['eslint:recommended'],
-    parserOptions: {
-      ecmaVersion: 6,
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    env: {
-      node: true,
+  },
+  env: {
+    node: true,
+  },
+  rules: {
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  settings: {
+    react: {
+      version: '16.12.0',
     },
-    rules: {
-      quotes: ['error', 'single', {
-        avoidEscape: true
-      }],
-      'comma-dangle': ['error', 'always-multiline'],
-    },
+  },
 };
