2b1900335f5b9671a49aecbb9f6247528be16e2e
Eddy Hernandez <eddy.hernandez@immersivelabs.co.uk>
2019-12-14 14:19:51 +0000

    Add snapshot tests for CarouselSlide

--- src/tests/CarouselSlide.test.js
+++ src/tests/CarouselSlide.test.js
@@ -12,15 +12,6 @@ describe('Img', () => {
     mounted = mount(<Img src={imgUrl} imgHeight={500} />);
   });
 
-  it('renders and <img> with the given src', () => {
-    expect(mounted.containsMatchingElement(<img src={imgUrl} />)).toBe(true);
-  });
-
-  it('has the expected static styles', () => {
-    expect(mounted).toHaveStyleRule('width', '100%');
-    expect(mounted).toHaveStyleRule('object-fit', 'cover');
-  });
-
   it('uses imgHeight as the height style property', () => {
     expect(mounted).toHaveStyleRule('height', '500px');
     mounted.setProps({ imgHeight: 'calc(100vh -  100px)' });
@@ -46,6 +37,10 @@ describe('Img', () => {
     expect(mounted.find(TestImg)).toHaveStyleRule('height', 'auto');
     expect(mounted.find(TestImg)).toHaveStyleRule('object-fit', 'fill');
   });
+
+  it('renders correctly', () => {
+    expect(mounted.find('img')).toMatchSnapshot();
+  });
 });
 
 describe('Carousel Slide', () => {
@@ -60,32 +55,6 @@ describe('Carousel Slide', () => {
     );
   });
 
-  it('renders a <figure>', () => {
-    expect(wrapper.type()).toBe('figure');
-  });
-
-  it('renders props.Img and a <figcaption> as children', () => {
-    expect(wrapper.childAt(0).type()).toBe(CarouselSlide.defaultProps.Img);
-    expect(wrapper.childAt(1).type()).toBe('figcaption');
-  });
-
-  it('passes `imgUrl` through to the <img>', () => {
-    const imgUrl = 'https://example.com/image.png';
-    wrapper.setProps({ imgUrl });
-    const img = wrapper.find(CarouselSlide.defaultProps.Img);
-    expect(img.prop('src')).toBe(imgUrl);
-  });
-
-  it('uses `description` and `attribution` as the <figcaption>', () => {
-    const description = 'A jaw-dropingly spectacular image';
-    const attribution = 'Trevor Burnham';
-    wrapper.setProps({ description, attribution });
-    expect(wrapper.find('figcaption').text()).toBe(
-      `${description} ${attribution}`
-    );
-    expect(wrapper.find('figcaption strong').text()).toBe(description);
-  });
-
   it('passes other props through to the <figure>', () => {
     const style = {};
     const onClick = () => {};
@@ -94,5 +63,14 @@ describe('Carousel Slide', () => {
     expect(wrapper.prop('style')).toBe(style);
     expect(wrapper.prop('onClick')).toBe(onClick);
     expect(wrapper.prop('className')).toBe(className);
+  });
+
+  it('renders correctly', () => {
+    wrapper.setProps({
+      description: 'Description',
+      attribution: 'Attribution',
+    });
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
